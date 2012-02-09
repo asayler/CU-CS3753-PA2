2a5a2e5ee4ecaa935fe2fb70845302bcd79beab8
Andy Sayler <andy.sayler@gmail.com>
2012-02-08 18:05:40 -0700

    Added pthread-hello example

--- Makefile
+++ Makefile
@@ -1,15 +1,18 @@
 CC = gcc
 CFLAGS = -c -g -Wall -Wextra
-LFLAGS = -Wall -Wextra
+LFLAGS = -Wall -Wextra -pthread
 
 .PHONY: all clean
 
-all: lookup queueTest
+all: lookup queueTest pthread-hello
 
 lookup: lookup.o queue.o util.o
 	$(CC) $(LFLAGS) $^ -o $@
 
 queueTest: queueTest.o queue.o
+	$(CC) $(LFLAGS) $^ -o $@
+
+pthread-hello: pthread-hello.o
 	$(CC) $(LFLAGS) $^ -o $@
 
 lookup.o: lookup.c
@@ -24,8 +27,11 @@ queue.o: queue.c queue.h
 util.o: util.c util.h
 	$(CC) $(CFLAGS) $<
 
+pthread-hello.o: pthread-hello.c
+	$(CC) $(CFLAGS) $<
+
 clean:
-	rm -f lookup queueTest
+	rm -f lookup queueTest pthread-hello
 	rm -f *.o
 	rm -f *~
 	rm -f results.txt
---
+++ pthread-hello.c
@@ -0,0 +1,74 @@
+/******************************************************************************
+ * FILE: pthread_hello.c
+ * DESCRIPTION:
+ *   A "hello world" Pthreads program.  Demonstrates thread creation and
+ *   termination.
+ * AUTHOR: Blaise Barney, Junho Ahn, Andy Sayler
+ * LAST REVISED: 02/06/12
+ ******************************************************************************/
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define NUM_THREADS	5
+
+/* Function for Each Thread to Run */
+void* PrintHello(void* threadid)
+{
+    /* Setup Local Vars and Handle void* */
+    long* tid = threadid;
+    long t;
+    long numprint = 3;
+
+    /* Print hello numprint times */
+    for(t=0; t<numprint; t++)
+	{
+	    printf("Hello World! It's me, thread #%ld! "
+		   "This is printout %ld of %ld\n",
+		   *tid, (t+1), numprint);
+	    /* Sleep for 1 to 2 Seconds */
+	    usleep((rand()%100)*10000+1000000);
+	}
+    
+    /* Exit, Returning NULL*/
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    /* void unused vars */
+    (void) argc;
+    (void) argv;
+
+    /* Setup Local Vars */
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long t;
+    long cpyt[NUM_THREADS];
+    
+    /* Spawn NUM_THREADS threads */
+    for(t=0;t<NUM_THREADS;t++){
+	printf("In main: creating thread %ld\n", t);
+	cpyt[t] = t;
+	rc = pthread_create(&(threads[t]), NULL, PrintHello, &(cpyt[t]));
+	if (rc){
+	    printf("ERROR; return code from pthread_create() is %d\n", rc);
+	    exit(EXIT_FAILURE);
+	}
+    }
+    
+    /* Wait for All Theads to Finish */
+    for(t=0;t<NUM_THREADS;t++){
+	pthread_join(threads[t],NULL);
+    }
+    printf("All of the threads were completed!\n");
+    
+
+    /* Last thing that main() should do */
+    /* pthread_exit unnecessary due to previous join */ 
+    //pthread_exit(NULL);
+
+    return 0;
+}
